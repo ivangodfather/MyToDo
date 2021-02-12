4481549f66669472b100fbef0ec9016656431d2f
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2021-02-12 07:59:38 +0100

    first commit

---
+++ MyToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyToDo
+//
+//  Created by Ivan Ruiz Monjo on 12/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyToDo/MyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoApp.swift
+//  MyToDo
+//
+//  Created by Ivan Ruiz Monjo on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
